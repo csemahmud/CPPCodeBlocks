815ee7dce9d66781d8ef93f97e38fe8dee952b51
KHAN MAHMUDUL HASAN(ITAGE) <24-253G@itage.local>
2024-10-15 09:41:36 +0900

    2024-10-15 added P03_AtCoder_20241015 from office windows .

---
+++ P03_AtCoder_20241015/main.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+///* Author : KHAN MAHMUDUL HASAN CSE BD JP */
+
+int main()
+{
+    for(int i = 0; i < 26; i++){
+
+        int n;
+        cin >> n;
+
+        switch(n){
+        case 1:
+            cout << "a";
+            break;
+        case 2:
+            cout << "b";
+            break;
+        case 3:
+            cout << "c";
+            break;
+        case 4:
+            cout << "d";
+            break;
+        case 5:
+            cout << "e";
+            break;
+        case 6:
+            cout << "f";
+            break;
+        case 7:
+            cout << "g";
+            break;
+        case 8:
+            cout << "h";
+            break;
+        case 9:
+            cout << "i";
+            break;
+        case 10:
+            cout << "j";
+            break;
+        case 11:
+            cout << "k";
+            break;
+        case 12:
+            cout << "l";
+            break;
+        case 13:
+            cout << "m";
+            break;
+        case 14:
+            cout << "n";
+            break;
+        case 15:
+            cout << "o";
+            break;
+        case 16:
+            cout << "p";
+            break;
+            break;
+        case 17:
+            cout << "q";
+            break;
+        case 18:
+            cout << "r";
+            break;
+        case 19:
+            cout << "s";
+            break;
+        case 20:
+            cout << "t";
+            break;
+        case 21:
+            cout << "u";
+            break;
+        case 22:
+            cout << "v";
+            break;
+        case 23:
+            cout << "w";
+            break;
+        case 24:
+            cout << "x";
+            break;
+        case 25:
+            cout << "y";
+            break;
+        case 26:
+            cout << "z";
+            break;
+        }
+
+    }
+    return 0;
+}
+
+/*
+
+Sample input 1
+
+1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+
+Sample output 1
+
+abcdefghijklmnopqrstuvwxyz
+
+Sample input 2
+
+2 1 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26
+
+Sample output 2
+
+bacdefghijklmnopqrstuvwxyz
+
+Sample input 3
+
+5 11 12 16 25 17 18 1 7 10 4 23 20 3 2 24 26 19 14 9 6 22 8 13 15 21
+
+Sample output 3
+
+eklpyqragjdwtcbxzsnifvhmou
+
+*/
